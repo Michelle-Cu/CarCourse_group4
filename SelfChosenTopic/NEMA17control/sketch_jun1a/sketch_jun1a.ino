890e3d58b270854d270cfa81bda2d887571308b3
Alex <alex.yuanhsi.lin@gmail.com>
2026-06-01 17:04:20 +0800

    old punch code

--- SelfChosenTopic/NEMA17control/sketch_jun1a/sketch_jun1a.ino
+++ SelfChosenTopic/NEMA17control/sketch_jun1a/sketch_jun1a.ino
@@ -1,7 +1,7 @@
 #include <AccelStepper.h>
 
-const int stepPin = 8; 
-const int dirPin = 9;  
+const int stepPin = 14; 
+const int dirPin = 12;  
 #define motorInterfaceType 1
 AccelStepper stepper(motorInterfaceType, stepPin, dirPin);
 
@@ -31,7 +31,7 @@ void loop() {
     Serial.println(input);
     
     // 2. If the user types START, run the sequential moves
-    if (input == "START") { 
+    if (1) { 
       Serial.println("Starting movement sequence...");
       
       // Move 90 degrees in negative direction at 400 steps/sec
