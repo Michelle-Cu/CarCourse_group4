ae9f7aa727351bbad20d4bc8118e030218e62193
Stanley_Hsu <stanleyganger@gmail.com>
2026-06-16 16:29:43 +0800

    correct pin for sound module

--- SelfChosenTopic/esp32/sound_mod/sound_mod.ino
+++ SelfChosenTopic/esp32/sound_mod/sound_mod.ino
@@ -4,12 +4,12 @@
 DFRobotDFPlayerMini myDFPlayer;
 
 void setup() {
-  Serial1.begin(9600);   // Talk to DFPlayer Mini (Pins 18/19)
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);   // Talk to DFPlayer Mini (RX=GPIO16, TX=GPIO17)
   Serial.begin(115200);  // Talk to Serial Monitor on your computer
 
   Serial.println(F("Initializing DFPlayer..."));
 
-  if (!myDFPlayer.begin(Serial1)) {  
+  if (!myDFPlayer.begin(Serial2)) {  
     Serial.println(F("Unable to begin. Please check wiring/SD card!"));
     while(true);
   }
